801a791195b2a927f712fc2e7fde5f02e27d89b4
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2020-11-28 12:31:32 +0000

    Fix lua generation bug added particle physics system

---
+++ src/main/java/com/boc_dev/lge_systems/physics/ParticleSystem.java
@@ -0,0 +1,90 @@
+package com.boc_dev.lge_systems.physics;
+
+import com.boc_dev.lge_model.gcs.Component;
+import com.boc_dev.lge_model.gcs.Registry;
+import com.boc_dev.lge_model.generated.components.*;
+import com.boc_dev.lge_model.systems.GcsSystem;
+import com.boc_dev.physics_library.particle_system_dynamics_verbose.*;
+import com.boc_dev.physics_library.rigid_body_dynamics_verbose.RigidBody;
+
+import java.util.*;
+
+public class ParticleSystem implements GcsSystem<ParticleBodyObject> {
+
+	private final ParticleSimulation particleSimulation = new ParticleSimulation();
+
+	@Override
+	public void update(long time, HashSet<ParticleBodyObject> particleBodyObjects, Registry registry) {
+
+		if (time > 1000) {
+
+			ArrayList<Particle> particles = new ArrayList<>();
+
+			for (ParticleBodyObject particleBodyObject : particleBodyObjects) {
+
+				// get parent transform
+				if (particleBodyObject.getParent() != null && particleBodyObject.getParent().getComponentType().equals(ComponentType.TRANSFORM)) {
+
+					TransformObject transformObject = (TransformObject) particleBodyObject.getParent();
+
+					// get forces
+					ArrayList<NaryForce> forces = new ArrayList<>();
+					for (Component child : particleBodyObject.getChildren()) {
+						if (child.getComponentType().equals(ComponentType.PARTICLESIMPLEGRAVITY)) {
+							forces.add(new SimpleGravity(((ParticleSimpleGravityObject) child).getG()));
+						} else if (child.getComponentType().equals(ComponentType.PARTICLESPRING)) {
+							ParticleSpringObject spring = (ParticleSpringObject) child;
+							forces.add(new Spring(spring.getRestLength(), spring.getSpringConstant(), spring.getDampingConstant()));
+						} else if (child.getComponentType().equals(ComponentType.PARTICLEVISCOUSDRAG)) {
+							forces.add(new ViscousDrag(((ParticleViscousDragObject) child).getCoefficientOfDrag()));
+						}
+					}
+
+					particles.add(new Particle(
+							particleBodyObject.getUuid(),
+							particleBodyObject.getMass(),
+							transformObject.getPosition().toVecd(),
+							particleBodyObject.getVelocity(),
+							forces
+					));
+
+				}
+			}
+
+			particleSimulation.iterate(0.02, particles, new ArrayList());
+
+			Iterator<Particle> particleIterator = particles.iterator();
+			while (particleIterator.hasNext()) {
+
+				Particle next = particleIterator.next();
+
+				// find particle in sim
+				for (ParticleBodyObject particleBodyObject : particleBodyObjects) {
+
+					if (next.getUuid().equals(particleBodyObject.getUuid())) {
+
+						particleBodyObject.getUpdater().setVelocity(next.getVelocity()).sendUpdate();
+
+						if (particleBodyObject.getParent() != null && particleBodyObject.getParent().getComponentType().equals(ComponentType.TRANSFORM)) {
+
+							TransformObject transformObject = (TransformObject) particleBodyObject.getParent();
+
+							transformObject.getUpdater()
+									.setPosition(next.getPosition().toVec3f())
+									.sendUpdate();
+						}
+					}
+
+				}
+
+				particleIterator.remove();
+			}
+		}
+
+	}
+
+	@Override
+	public ComponentType getTypeSystemUpdates() {
+		return ComponentType.PARTICLEBODY;
+	}
+}
